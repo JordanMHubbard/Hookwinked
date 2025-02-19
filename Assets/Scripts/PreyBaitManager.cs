2ea1f8af034e295643fea51a1fd501aeaa21dde1
Jordan <156966171+JordanMHubbard@users.noreply.github.com>
2025-02-19 13:58:17 -0800

    Created water shader with simulated waves.

--- Assets/Scripts/PreyBaitManager.cs
+++ Assets/Scripts/PreyBaitManager.cs
@@ -4,6 +4,7 @@ public class PreyBaitManager : MonoBehaviour
 {
     [SerializeField] private float pointValue = 100f;
     [SerializeField] private float energyValue = 10f;
+    private bool isPrey = true;
 
     public float GetPointValue()
     {
@@ -13,5 +14,10 @@ public class PreyBaitManager : MonoBehaviour
     public float GetEnergyValue()
     {
         return energyValue;
+    }
+
+    public bool GetIsPrey()
+    {
+        return isPrey;
     }
 }
