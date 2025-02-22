d766d423c931ff5047360c9f42dbef4dbaf6a34e
Jordan <156966171+JordanMHubbard@users.noreply.github.com>
2025-02-21 18:12:59 -0800

    Created basic fish AI movement and obstacle avoidance.

--- Assets/Scripts/AIFishController.cs
+++ Assets/Scripts/AIFishController.cs
@@ -1,14 +1,22 @@
 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
 public class AIFishController : MonoBehaviour
 {
     private bool hasTarget;
     private bool iTurning;
     private bool foundPrey;
+    private bool foundObstacle;
+    private List<string> currentObstacles;
+    private Vector3 repelDirection;
     private Vector3 targetLocation;
     private Vector3 currentVelocity;
     [SerializeField] private float swimSpeed = 3f;
+    private float viewDistance = 5f;
+    [SerializeField] private LayerMask interactableLayer;
 
     private CharacterController characterController;
 
@@ -22,20 +30,23 @@ public class AIFishController : MonoBehaviour
             Debug.LogWarning($"{gameObject.name}: characterController has not been set!");
             enabled = false;
         }
+
+        currentObstacles = new List<string>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Debug.DrawLine(transform.position, transform.position + transform.forward * 2f, Color.blue);
 
         if (!hasTarget)
         {
             hasTarget = FindTarget();
         }
-        else 
+        else if (hasTarget) 
         {
-            RotateNPC();
+            Rotate();
             MoveTowardsTarget();
         }
     }
@@ -58,10 +69,20 @@ public class AIFishController : MonoBehaviour
         return true;
     }
 
-    private void RotateNPC()
-    {
-        float turnSpeed = swimSpeed * Random.Range(1f, 3f);
-        Vector3 lookAt = targetLocation - transform.position;
+    private void Rotate()
+    {   
+        float turnSpeed = swimSpeed * Random.Range(1f, 2f);
+        Vector3 lookAt;
+
+        if (foundObstacle)
+        {
+            lookAt = repelDirection;
+        }
+        else
+        {
+           lookAt = targetLocation - transform.position;
+        }
+
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookAt), turnSpeed * Time.deltaTime);
     }
 
@@ -82,4 +103,79 @@ public class AIFishController : MonoBehaviour
             hasTarget = false;
         }
     }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == gameObject)
+        {
+            return;
+        }
+        
+        if (other.CompareTag("Obstacle"))
+        {
+            AvoidObstacle(other.gameObject);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        removeObstacle(other);
+    }
+
+    private void AvoidObstacle(GameObject obstacle)
+    {
+        if (obstacle == null) return;
+
+        Vector3 obstaclePosition = obstacle.transform.position;
+        Vector3 directionToObstacle = obstaclePosition - transform.position; 
+
+        Ray ray = new Ray(transform.position, directionToObstacle);
+        Debug.DrawLine(transform.position, transform.position + directionToObstacle * viewDistance, Color.red);
+        bool shouldAvoid = Physics.Raycast(ray, out RaycastHit hit, viewDistance, interactableLayer);
+    
+        if (shouldAvoid)
+        {
+            if (!currentObstacles.Contains(obstacle.name))
+            {
+                currentObstacles.Add(obstacle.name);
+            }
+
+            repelDirection = hit.normal * 2f; 
+            
+            if (!foundObstacle)
+            {
+                foundObstacle = true;
+            }
+        }
+        else 
+        {
+            removeObstacle(obstacle.GetComponent<Collider>());
+        }   
+        
+    }
+
+    private void removeObstacle(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            if (currentObstacles.Contains(other.gameObject.name))
+            {
+                currentObstacles.Remove(other.gameObject.name);
+            }
+            
+            if (currentObstacles.Count == 0)
+            {
+                StartCoroutine(unsetFoundObstacle());
+            }
+        }
+    }
+
+    IEnumerator unsetFoundObstacle()
+    {
+        yield return new WaitForSeconds(1f);
+        foundObstacle = false;
+    }
+
+
 }
