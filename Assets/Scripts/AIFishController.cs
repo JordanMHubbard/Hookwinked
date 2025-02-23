0d5e1153c0275b548966264d641ef6aa8acc8f8b
Jordan <156966171+JordanMHubbard@users.noreply.github.com>
2025-02-22 19:35:29 -0800

    Greatly improved AI steering and obstacle avoidance.

--- Assets/Scripts/AIFishController.cs
+++ Assets/Scripts/AIFishController.cs
@@ -1,19 +1,14 @@
-
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.Utilities;
 
 public class AIFishController : MonoBehaviour
 {
     private bool hasTarget;
-    private bool iTurning;
+    private bool isRepelling;
     private bool foundPrey;
-    private bool foundObstacle;
-    private List<string> currentObstacles;
+    private List<GameObject> currentObstacles;
     private Vector3 repelDirection;
     private Vector3 targetLocation;
-    private Vector3 currentVelocity;
     [SerializeField] private float swimSpeed = 3f;
     private float viewDistance = 5f;
     [SerializeField] private LayerMask interactableLayer;
@@ -31,7 +26,7 @@ public class AIFishController : MonoBehaviour
             enabled = false;
         }
 
-        currentObstacles = new List<string>();
+        currentObstacles = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -46,9 +41,24 @@ public class AIFishController : MonoBehaviour
         }
         else if (hasTarget) 
         {
+            IsObstacleInTargetDirection();
             Rotate();
             MoveTowardsTarget();
         }
+    }
+
+    bool IsAreaBlocked(Vector3 checkPosition, Vector3 boxSize)
+    {
+        Collider[] colliders = Physics.OverlapBox(checkPosition, boxSize * 0.5f, Quaternion.identity, interactableLayer);
+        foreach (Collider col in colliders)
+        {
+            if (col.CompareTag("Obstacle"))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private bool FindTarget()
@@ -60,10 +70,15 @@ public class AIFishController : MonoBehaviour
         else 
         {
             // Later need to consider 1.unreachable points, 2.only getting points within a defined space 
-            Vector3 locationOffset = new Vector3 (Random.Range(-5,5), 0, Random.Range(-5,5));
+            Vector3 locationOffset = new Vector3 (Random.Range(-10,10), 0, Random.Range(-10,10));
             targetLocation = transform.position + locationOffset;
             Debug.Log("targetLocation = " + targetLocation);
-            hasTarget = true;
+
+            if (IsAreaBlocked(targetLocation, new Vector3(2, 2, 2)))
+            {
+                Debug.Log("Area blocked!");
+                return false;
+            } 
         }
         
         return true;
@@ -71,18 +86,22 @@ public class AIFishController : MonoBehaviour
 
     private void Rotate()
     {   
+        //if (isRepelling) return;
+        
+        
         float turnSpeed = swimSpeed * Random.Range(1f, 2f);
+        Vector3 targetDirection = targetLocation - transform.position;
         Vector3 lookAt;
-
-        if (foundObstacle)
+        if (isRepelling) 
         {
+            Debug.Log("We are repelling now");
             lookAt = repelDirection;
+            Debug.DrawLine(transform.position, transform.position + repelDirection * viewDistance, Color.red);
         }
-        else
-        {
-           lookAt = targetLocation - transform.position;
+        else{
+            lookAt = targetDirection;
         }
-
+        //Vector3 lookAt = isRepelling ? (targetDirection + repelDirection) / 2 : targetDirection;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookAt), turnSpeed * Time.deltaTime);
     }
 
@@ -113,69 +132,97 @@ public class AIFishController : MonoBehaviour
         
         if (other.CompareTag("Obstacle"))
         {
-            AvoidObstacle(other.gameObject);
+            AvoidObstacle(other);
         }
         
     }
 
-    void OnTriggerExit(Collider other)
+    private void AvoidObstacle(Collider other)
     {
-        removeObstacle(other);
-    }
-
-    private void AvoidObstacle(GameObject obstacle)
-    {
+        GameObject obstacle = other.gameObject;
         if (obstacle == null) return;
+
+        // When overlapping obstacle, check if hit ray more closely aligns with left direction or right direction,
+        // and choose the opposite direction to turn towards
 
         Vector3 obstaclePosition = obstacle.transform.position;
         Vector3 directionToObstacle = obstaclePosition - transform.position; 
-
+        //Debug.DrawLine(transform.position, transform.position + directionToObstacle * viewDistance, Color.red);
         Ray ray = new Ray(transform.position, directionToObstacle);
-        Debug.DrawLine(transform.position, transform.position + directionToObstacle * viewDistance, Color.red);
-        bool shouldAvoid = Physics.Raycast(ray, out RaycastHit hit, viewDistance, interactableLayer);
-    
-        if (shouldAvoid)
-        {
-            if (!currentObstacles.Contains(obstacle.name))
-            {
-                currentObstacles.Add(obstacle.name);
-            }
+        Physics.Raycast(ray, out RaycastHit hit, viewDistance, interactableLayer);
 
-            repelDirection = hit.normal * 2f; 
-            
-            if (!foundObstacle)
-            {
-                foundObstacle = true;
-            }
-        }
-        else 
-        {
-            removeObstacle(obstacle.GetComponent<Collider>());
-        }   
+       
+        Vector3 leftDir = Quaternion.Euler(0, -45, 0) * transform.forward;
+        Vector3 rightDir = Quaternion.Euler(0, 45, 0) * transform.forward;
+        Debug.DrawLine(transform.position, transform.position + leftDir * viewDistance, Color.green);
+        Debug.DrawLine(transform.position, transform.position + rightDir * viewDistance, Color.green);
         
+        float dotL = Vector3.Dot(Vector3.Normalize(directionToObstacle), Vector3.Normalize(leftDir));
+        float angleL = Mathf.Acos(dotL) * Mathf.Rad2Deg;
+
+        float dotR = Vector3.Dot(Vector3.Normalize(directionToObstacle), Vector3.Normalize(rightDir));
+        float angleR = Mathf.Acos(dotR) * Mathf.Rad2Deg;
+
+        if (angleL < angleR)
+        {
+            repelDirection = rightDir;
+            Debug.Log("Going Right");
+        }
+        else
+        {
+            repelDirection = leftDir;
+            Debug.Log("Going Left");
+        }
+
+        if (!isRepelling) isRepelling = true;
     }
 
-    private void removeObstacle(Collider other)
+    private void IsObstacleInTargetDirection()
+    {
+        //Maybe fix this so it covers a wider range of area?
+        
+        if (!isRepelling) 
+        {
+            //Debug.Log("I will not check if obstacle in target direction");
+            return;
+        }
+        
+        Vector3 desiredDirection = targetLocation - transform.position; 
+        Ray ray = new Ray(transform.position, desiredDirection);
+        Debug.DrawLine(transform.position, transform.position + desiredDirection * viewDistance, Color.magenta);
+        bool obstacleInWay = Physics.Raycast(ray, out RaycastHit hit, viewDistance, interactableLayer);
+
+        if (obstacleInWay) 
+        {
+            //AddObstacle(hit.collider);
+            return;
+        }
+
+        //RemoveObstacle(hit.collider);
+        isRepelling = false; 
+    }
+
+
+    void AddObstacle(Collider other)
     {
         if (other.CompareTag("Obstacle"))
         {
-            if (currentObstacles.Contains(other.gameObject.name))
+            if (!currentObstacles.Contains(other.gameObject))
             {
-                currentObstacles.Remove(other.gameObject.name);
-            }
-            
-            if (currentObstacles.Count == 0)
-            {
-                StartCoroutine(unsetFoundObstacle());
+                currentObstacles.Add(other.gameObject);
             }
         }
     }
 
-    IEnumerator unsetFoundObstacle()
+    void RemoveObstacle(Collider other)
     {
-        yield return new WaitForSeconds(1f);
-        foundObstacle = false;
+        if (other.CompareTag("Obstacle"))
+        {
+            if (currentObstacles.Contains(other.gameObject))
+            {
+                currentObstacles.Remove(other.gameObject);
+            }
+        }
     }
-
 
 }
