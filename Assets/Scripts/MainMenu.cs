b426c58bfa19cd54078635d37dd554dd232d3fda
Jordan <156966171+JordanMHubbard@users.noreply.github.com>
2025-03-24 16:08:35 -0700

    Added basic functionality to main menu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        
+        SceneManager.LoadScene("TheReef");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
